bf8fa9596ff132f691ac788ba73e4424a9e90704
billyvicencio <billyvicencio123@gmail.com>
2026-09-02 11:28:56 +0800

    Feat: Implement CRUD operations in ProductController

--- example-app/app/Http/Controllers/ProductController.php
+++ example-app/app/Http/Controllers/ProductController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        return Product::all();
+    }
+
+    public function store(Request $request)
+    {
+        return Product::create($request->validate([
+            'name' => 'required|string',
+            'price' => 'required|integer',
+        ]));
+    }
+
+    public function update(Request $request, Product $product)
+    {
+        $product->update($request->only(['name', 'price']));
+        return $product;
+    }
+
+    public function destroy(Product $product)
+    {
+        $product->delete();
+        return response()->noContent();
+    }
 }
