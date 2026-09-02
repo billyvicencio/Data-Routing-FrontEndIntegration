553d42f3642131689320935744cd6be86a88c346
billyvicencio <billyvicencio123@gmail.com>
2026-09-02 11:58:48 +0800

    Feat: Update ProductController to use Inertia for rendering products and add HandleInertiaRequests middleware

--- example-app/app/Http/Controllers/ProductController.php
+++ example-app/app/Http/Controllers/ProductController.php
@@ -3,32 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
-use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class ProductController extends Controller
 {
     public function index()
     {
-        return Product::all();
-    }
-
-    public function store(Request $request)
-    {
-        return Product::create($request->validate([
-            'name' => 'required|string',
-            'price' => 'required|integer',
-        ]));
-    }
-
-    public function update(Request $request, Product $product)
-    {
-        $product->update($request->only(['name', 'price']));
-        return $product;
-    }
-
-    public function destroy(Product $product)
-    {
-        $product->delete();
-        return response()->noContent();
+        return Inertia::render('Products/Index', [
+            'products' => Product::all(),
+        ]);
     }
 }
