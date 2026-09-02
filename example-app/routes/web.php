00a74c2a49ebd1abc8657b01400910c49cc161f6
billyvicencio <billyvicencio123@gmail.com>
2026-09-02 11:24:12 +0800

    Added: Add product routes to web.php and remove the md file in first commit

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::post('/products', [ProductController::class, 'store']);
