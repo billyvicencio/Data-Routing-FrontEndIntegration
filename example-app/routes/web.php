4f446ac12f8547e69352dcf6e72bf02280832eb8
billyvicencio <billyvicencio123@gmail.com>
2026-09-02 12:58:53 +0800

    Redirect homepage route to tasks

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -16,3 +16,4 @@ Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
 Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
 Route::patch('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
 Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
+Route::get('/', [TaskController::class, 'index']);
